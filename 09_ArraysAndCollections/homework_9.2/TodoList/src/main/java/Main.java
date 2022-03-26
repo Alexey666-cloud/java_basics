f6918eb8bdf7f707ea3dea07d6e67405d3767902
Alexey <alexendr-makedonsky@yandex.ru>
2022-03-26 13:45:20 +0300

    addTodoList

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
-    private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
+        TodoList todoList = new TodoList();
+
         // TODO: написать консольное приложение для работы со списком дел todoList
+
+        String firstLine = "";
+        int index = 0;
+        String secondLine = "";
+
+        Scanner scanner = new Scanner(System.in);
+        String nextLine = scanner.nextLine();
+        String[] array = nextLine.split(" ");
+        String newCommand = array[0];
+        String regex = "[aA-zZ|0-9]+";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(array[1]);
+
+        if (matcher.find()) {
+            firstLine = array[1];
+            index = Integer.parseInt(firstLine);
+            nextLine.replace(firstLine, "");
+
+            if (newCommand.equals("ADD")) {
+                todoList.add(secondLine);
+            }
+            System.out.println("Добавлено дело: " + index + " " + secondLine.trim());
+        }
     }
 }
--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -2,28 +2,48 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> todoList = new ArrayList<>();
+
     public void add(String todo) {
         // TODO: добавьте переданное дело в конец списка
+        todoList.add(todo);
     }
 
     public void add(int index, String todo) {
         // TODO: добавьте дело на указаный индекс,
         //  проверьте возможность добавления
+        if (!todo.isEmpty()) {
+            if (index >= 0 && index < todoList.size()) {
+                todoList.add(index, todo);
+            } else todoList.add(todo);
+        } else return;
+
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+        if (!todo.isEmpty()) {
+            if (index >= 0 && index < todoList.size()) {
+                todoList.remove(index);
+                todoList.add(index, todo);
+            } else return;
+        }
     }
 
     public void delete(int index) {
         // TODO: удалить дело находящееся по переданному индексу,
         //  проверьте возможность удаления дела
+        if (index >= 0 && index < todoList.size()) {
+            todoList.remove(index);
+        } else return;
     }
 
     public ArrayList<String> getTodos() {
         // TODO: вернуть список дел
-        return new ArrayList<>();
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println(todoList.get(i));
+        }
+        return todoList;
     }
-
 }
