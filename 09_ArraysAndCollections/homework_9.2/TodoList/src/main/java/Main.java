fcebc5cf577617def07237613ab90845b8785ce2
Alex Kudryashov <alex.k@sociallinks.io>
2022-03-26 21:25:25 +0300

    fixBagTodoList

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -1,6 +1,4 @@
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
 
@@ -9,27 +7,58 @@ public class Main {
 
         // TODO: написать консольное приложение для работы со списком дел todoList
 
-        String firstLine = "";
-        int index = 0;
-        String secondLine = "";
+        System.out.println("Введите в консоль команды, для выполнения действия: "
+                + "\n1 - LIST - вывод списка дел"
+                + "\n2 - ADD - создать дело"
+                + "\n3 - EDIT - редактировать дело"
+                + "\n4 - DELETE - Удалить дело");
 
         Scanner scanner = new Scanner(System.in);
-        String nextLine = scanner.nextLine();
-        String[] array = nextLine.split(" ");
-        String newCommand = array[0];
-        String regex = "[aA-zZ|0-9]+";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(array[1]);
 
-        if (matcher.find()) {
-            firstLine = array[1];
-            index = Integer.parseInt(firstLine);
-            nextLine.replace(firstLine, "");
+        while (true) {
+            String nextLine = scanner.nextLine();
+            String[] inputStringArray = nextLine.split(" ");
+            String command = inputStringArray[0];
+            int size = 0;
+            String todo = "";
 
-            if (newCommand.equals("ADD")) {
-                todoList.add(secondLine);
+            if (command.equals("ADD")) {
+                size = todoList.todoList.size();
+                String[] todoArray = new String[inputStringArray.length - 1];
+                System.arraycopy(inputStringArray, 1, todoArray, 0, inputStringArray.length - 1);
+                todo = createTodoString(todoArray);
+                todoList.add(size + 1, todo);
+                System.out.println("Добавлено дело: " + todo);
             }
-            System.out.println("Добавлено дело: " + index + " " + secondLine.trim());
+            
+            if (command.equals("EDIT")) {
+                todoList.edit(todo, Integer.parseInt(inputStringArray[1]));
+                System.out.println("Дело перезаписано!");
+            }
+
+            if (command.equals("DELETE")) {
+                todoList.delete(Integer.parseInt(inputStringArray[1]));
+                System.out.println("Дело удалено!");
+            }
+
+            if (command.equals("LIST")) {
+                todoList.getTodos();
+            }
+            
+            if (command.equals("EXIT")) {
+                scanner.close();
+                System.exit(0);
+            }
         }
     }
+
+    private static String createTodoString(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            stringBuilder.append(args[i]);
+            stringBuilder.append(" ");
+        }
+        return stringBuilder.toString().trim();
+    }
 }
+
