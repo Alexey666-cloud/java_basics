05975b1be259af984fd8631aef5aaece5702708c
Alex Kudryashov <alex.k@sociallinks.io>
2022-03-27 20:47:23 +0300

    addEmailList

--- 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
+++ 09_ArraysAndCollections/homework_9.3/EmailList/src/main/java/EmailList.java
@@ -1,15 +1,28 @@
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EmailList {
 
+    TreeSet<String> emailList = new TreeSet<>();
+
     public void add(String email) {
+        String emailSeparator = ".";
+        String dogChar = "@";
+        Pattern upperСaseRegex = Pattern.compile("[A-Z]");
+        Matcher matcher = upperСaseRegex.matcher(email);
+
         // TODO: валидный формат email добавляется
+        if (email.contains(emailSeparator) && email.contains(dogChar)) {
+            emailList.add(email);
+        }
     }
 
     public List<String> getSortedEmails() {
         // TODO: возвращается список электронных адресов в алфавитном порядке
-        return Collections.emptyList();
+        return new ArrayList<>(emailList);
     }
 
 }
