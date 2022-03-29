a924458164316d610e3b3d23821f7ce6e2a4a479
Alex Kudryashov <alex.k@sociallinks.io>
2022-03-30 00:21:33 +0300

    addPhoneBook

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,37 +1,55 @@
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class PhoneBook {
+
+    public static String key = "";
+    public static String value = "";
+
+    public static TreeMap<String, String> phoneBook = new TreeMap<>();
 
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона (отдельные методы для проверки)
         // если такой номер уже есть в списке, то перезаписать имя абонента
+        key = name;
+        value = phone;
+
+        phoneBook.put(key, value);
     }
 
     public String getContactByPhone(String phone) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найдены - вернуть пустую строку
+        if (phoneBook.containsValue(value)) {
+            return  phoneBook.get(key) + " - " + phone;
+        }
         return "";
     }
 
     public Set<String> getContactByName(String name) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
+        if (phoneBook.containsKey(key)) {
+            return Collections.singleton(name + " - " + phoneBook.get(name));
+        }
         return new TreeSet<>();
     }
 
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
+        if (!phoneBook.containsKey(key) && !phoneBook.isEmpty()) {
+            return new TreeSet<String>(Set.of(key + " - " + value));
+        }
         return new TreeSet<>();
     }
 
     // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
     // это поможет вам найти все ключи (key) по значению (value)
-    /*
-        for (Map.Entry<String, String> entry : map.entrySet()){
-            String key = entry.getKey(); // получения ключа
-            String value = entry.getValue(); // получения ключа
+    private static void getMap(Map<String, String> map) {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            key = entry.getKey(); // получения ключа
+            value = entry.getValue(); // получения ключа
         }
-    */
+        System.out.println(key + " - " + value);
+    }
 }
