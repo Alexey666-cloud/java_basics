ce08bcfdfe60eacf4f13fbb76f73dc1ef69e9ae1
Alex Kudryashov <alex.k@sociallinks.io>
2022-03-31 14:11:45 +0300

    fixBagPhoneBook

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,19 +1,20 @@
 import java.util.*;
 
 public class PhoneBook {
+    String key = "";
+    String value = "";
 
-    public static String key = "";
-    public static String value = "";
-
-    public static TreeMap<String, String> phoneBook = new TreeMap<>();
+    TreeMap<String, String> phoneBook = new TreeMap<>();
 
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона (отдельные методы для проверки)
         // если такой номер уже есть в списке, то перезаписать имя абонента
-        key = name;
-        value = phone;
 
-        phoneBook.put(key, value);
+        if (!name.equals(phone) && !phone.equals(name)) {
+            key = name;
+            value = phone;
+            phoneBook.put(key, value);
+        }
     }
 
     public String getContactByPhone(String phone) {
@@ -30,7 +31,7 @@ public class PhoneBook {
     public Set<String> getContactByName(String name) {
         // формат одного контакта "Имя - Телефон"
         // если контакт не найден - вернуть пустой TreeSet
-        if (phoneBook.containsKey(key)) {
+        if (phoneBook.containsKey(name) && phoneBook.containsValue(value)) {
             return Collections.singleton(name + " - " + phoneBook.get(name));
         }
         return new TreeSet<>();
@@ -39,24 +40,27 @@ public class PhoneBook {
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
-        if (!phoneBook.containsKey(key) && !phoneBook.isEmpty()) {
-            return new TreeSet<String>(Set.of(key + " - " + value));
-        }
-        if (phoneBook.size() > 3 && !phoneBook.containsKey(value) && !phoneBook.isEmpty()) {
-            for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-                return Collections.singleton(key + " - " + entry.getValue() + ", " + phoneBook.get(key));
+
+        if (!phoneBook.containsKey(key) && !phoneBook.containsValue(value) && !phoneBook.isEmpty()) {
+            for (Map.Entry<String, String> contactList : phoneBook.entrySet()) {
+                return new TreeSet<String>(Set.of(contactList.getKey() + " - " + contactList.getValue()));
             }
         }
+            for (Map.Entry<String, String> twoPhone : phoneBook.entrySet()) {
+                if (value == twoPhone.getValue() && phoneBook.size() > 1) {
+                    return Collections.singleton(key + " - " + twoPhone.getValue());
+                }
+            }
         return new TreeSet<>();
     }
 
     // для обхода Map используйте получение пары ключ->значение Map.Entry<String,String>
     // это поможет вам найти все ключи (key) по значению (value)
-    private static void getMap(Map<String, String> map) {
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            key = entry.getKey(); // получения ключа
-            value = entry.getValue(); // получения ключа
-        }
-        System.out.println(key + " - " + value);
-    }
+//    private static void getMap(Map<String, String> map) {
+//        for (Map.Entry<String, String> entry : map.entrySet()) {
+//           String key = entry.getKey(); // получения ключа
+//           String value = entry.getValue(); // получения ключа
+//        }
+//        System.out.println(key + " - " + value);
+//    }
 }
