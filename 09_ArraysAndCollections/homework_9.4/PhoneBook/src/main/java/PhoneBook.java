03c3f108cd843c50fc3e435cbb613e0b0673490d
Alex Kudryashov <alex.k@sociallinks.io>
2022-04-04 20:44:53 +0300

    fixBagPhoneBook

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -9,14 +9,13 @@ public class PhoneBook {
         // если такой номер уже есть в списке, то перезаписать имя абонента
 
         for (Map.Entry<String, String> numb : phoneBook.entrySet()) {
-            if (phoneBook.containsValue(numb.getValue()) && name != phoneBook.get(name)) {
-                phoneBook.remove(name);
+            if (phoneBook.containsValue(numb.getValue() == phone)) {
+                phoneBook.remove(numb.getKey());
             }
         }
         if (!name.equals(phone) && !phone.equals(name)) {
             phoneBook.put(name, phone);
         }
-
     }
 
     public String getContactByPhone(String phone) {
@@ -45,21 +44,16 @@ public class PhoneBook {
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
 
         for (Map.Entry<String, String> contactList : phoneBook.entrySet()) {
-            if (phoneBook.containsValue(contactList.getValue()) == phoneBook.containsValue(contactList.getValue())
-                    && !contactList.getKey().isEmpty()
-                    && !contactList.getValue().isEmpty()) {
-                return new TreeSet<String>(Set.of(contactList.getKey()
-                        + " - " + contactList.getValue()));
-            }
-            if (phoneBook.containsKey(contactList.getKey()) == phoneBook.containsKey(contactList.getKey())
-                    && !contactList.getKey().isEmpty()
-                    && !contactList.getValue().isEmpty()) {
-                return new TreeSet<String>(Set.of(contactList.getKey()
-                        + " - " + contactList.getValue()
-                        + ", " + contactList.getValue()));
-            }
+//            if (phoneBook.size() >= 2
+//                    && phoneBook.containsKey(contactList.getKey()) == phoneBook.containsKey(contactList.getKey())
+//                    && !contactList.getKey().isEmpty()
+//                    && !contactList.getValue().isEmpty()) {
+//                return new TreeSet<String>(Set.of(contactList.getKey()
+//                        + " - " + contactList.getValue()
+//                        + ", " + contactList.getValue()));
+//            }
             if (!contactList.getKey().isEmpty() && !contactList.getValue().isEmpty()) {
-                return new TreeSet<String>(Set.of(contactList.getKey()
+                return new TreeSet<>(Set.of(contactList.getKey()
                         + " - " + contactList.getValue()));
             }
         }
