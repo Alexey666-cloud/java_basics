0e1b0b99bc1830e545a25d5448a1df4a2a5684f3
Alex Kudryashov <alex.k@sociallinks.io>
2022-04-10 01:22:03 +0300

    fixBagPhoneBook

--- 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
+++ 09_ArraysAndCollections/homework_9.4/PhoneBook/src/main/java/PhoneBook.java
@@ -1,16 +1,21 @@
 import java.util.*;
 
 public class PhoneBook {
-
     TreeMap<String, String> phoneBook = new TreeMap<>();
+
 
     public void addContact(String phone, String name) {
         // проверьте корректность формата имени и телефона (отдельные методы для проверки)
         // если такой номер уже есть в списке, то перезаписать имя абонента
 
+
         for (Map.Entry<String, String> numb : phoneBook.entrySet()) {
             if (numb.getValue() == phone) {
                 phoneBook.remove(numb.getKey());
+            }
+            if (numb.getKey() == name) {
+                String oldPhone = phoneBook.get(name);
+                phoneBook.replace(name, oldPhone, oldPhone.concat(", " + phone));
             }
         }
         if (!name.equals(phone) && !phone.equals(name)) {
@@ -42,6 +47,7 @@ public class PhoneBook {
     public Set<String> getAllContacts() {
         // формат одного контакта "Имя - Телефон"
         // если контактов нет в телефонной книге - вернуть пустой TreeSet
+
         for (Map.Entry<String, String> contactList : phoneBook.entrySet()) {
             if (!contactList.getKey().isEmpty() && !contactList.getValue().isEmpty()) {
                 return new TreeSet<>(Set.of(contactList.getKey()
@@ -51,27 +57,3 @@ public class PhoneBook {
         return new TreeSet<>();
     }
 }
-
-//    Map<String, Set<String>> tempMap = new TreeMap<>();
-//    Set<String> result = new TreeSet<>();
-//        if (!phoneBook.isEmpty()) {
-//                for (Map.Entry<String, String> contactLine : phoneBook.entrySet()) {
-//        if (!phoneBook.isEmpty()) {
-//        Set<String> temp = new TreeSet<>();
-//        for (Map.Entry<String, String> entry : phoneBook.entrySet()) {
-//        String name = entry.getKey();
-//        String phone = entry.getValue();
-//        if (tempMap.containsKey(name)) {
-//        tempMap.get(name).add(phone);
-//        tempMap.put(phone, temp);
-//        } else {
-//        temp.add(phone);
-//        tempMap.put(name, temp);
-//        }
-//        }
-//        for (Map.Entry<String, Set<String>> entry : tempMap.entrySet()) {
-//        result.add(entry.getKey() + " - " + String.join(", ", entry.getValue()));
-//        }
-//        }
-//        }
-//        }
